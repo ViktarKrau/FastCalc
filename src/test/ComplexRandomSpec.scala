a8c6fa65cea0187fb2c42b22efae97544f4e6a00
Viktor <kwr@nxt.ru>
2015-11-22 20:35:58 +0300

    Added complex random numbers addition test

--- src/test/ComplexRandomSpec.scala
+++ src/test/ComplexRandomSpec.scala
@@ -1,4 +1,19 @@
 package test
 
+import fastcalc.ComplexNumber
+
 class ComplexRandomSpec extends ComplexNumberSpec {
+  val TEST_COUNT = 1000
+
+  private def randomTest(expected : (ComplexNumber, ComplexNumber) => ComplexNumber,
+                          tested : (ComplexNumber, ComplexNumber) => ComplexNumber) = {
+    assertEqualComplex(TEST_COUNT, generateComplex, expected, generateComplex, tested)
+  }
+
+  "A ComplexNumber (x + iy)" should "return a complex number (" +
+    "x + u, yi + vi) after adding another compelex number (u, v)" in {
+    randomTest((a, b) => new ComplexNumber(a.getReal + b.getReal, a.getImaginary + b.getImaginary), (a, b) => a + b)
+  }
+
+
 }
